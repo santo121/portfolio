ff6d7aaf3f1f4082b727d0fc7bc9bb3f98759d9b
santo <santomartin111@gmail.com>
2020-09-16 21:26:28 +0530

    Updates

---
+++ precache-manifest.05273bc7008fb2e864af1e3b2f73270f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0eac64d668be6b4e8d422f01b39b2fd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "75c0c662b9afea963989",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "b81c5da4404ded0b9323",
+    "url": "/portfolio/static/js/2.97bb4f35.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/2.97bb4f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c0c662b9afea963989",
+    "url": "/portfolio/static/js/main.7f4561d3.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
